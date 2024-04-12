6dde2bcc1d14c36f3e520bf04f3818082577e86b
Andrew Bollard <bollard92@gmail.com>
2024-04-12 13:43:25 -0500

    added code for race dropdown

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const dropDown = document.querySelector(".dropdown-menu");
 
 function raceSelector() {
-fetch('https://www.dnd5eapi.co/api/races') // api for the get request
-    .then(response => response.json())
-    .then(data => console.log(data));
-    
+    fetch('https://www.dnd5eapi.co/api/races') // api for the get request
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            for (let i = 0; i<data.results.length; i++) {
+                const liTag = document.createElement("li")
+                liTag.textContent = data.results[i].name
+                dropDown.appendChild(liTag)
+            }
+        });
 }
-raceSelector(); 
+raceSelector();
 
