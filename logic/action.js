dca7e6c8cbbc60827b92f3dda2ddfe4e0e7ed1fb
CodeChef <joetomsim@gmail.com>
2024-04-15 21:33:56 -0500

    added text and debugged

--- logic/action.js
+++ logic/action.js
@@ -48,105 +48,102 @@ let isGameOver = false;
 let isNextLevel = false;
 let maxHealth = 200;
 
-
-
-
 let string = localStorage.getItem("character");
 let savedCharacter = JSON.parse(string) || [];
 
-
 const playerInfo = {
-  name: savedCharacter.characterName, 
+  name: savedCharacter.characterName,
   race: savedCharacter.race
 };
 
+alert ("You awake alone in a forest, with only your weapon by your side...");
 
-
-
-
-
-
-function attack(){
+function attack() {
   enemyHP = enemyHP - playerAP;
   console.log("attack");
   console.log("Enemy Health: " + enemyHP);
-
-  if(enemyHP <= 0){
+  if (enemyHP <= 0) {
     nextLevel();
   }
-
   enemyAttack();
-  
 }
 
-function heal(){
+function heal() {
   console.log("heal");
-  console.log("Player Health: " + playerHP);
+  console.log("Player Health:" + playerHP);
 
-  function healPlayer(){
+  function healPlayer() {
     playerHP = playerHP + playerHeal;
-
-    if(playerHP > maxHealth){
+    alert("You regain 25HP!");
+    if (playerHP > maxHealth) {
       playerHP = maxHealth;
     }
   }
 
-  if(playerHP < maxHealth){
+  if (playerHP < maxHealth) {
     healPlayer()
-  }else{
+  } else {
     alert("You're already at max health")
   }
-
   enemyAttack();
-
 }
 
-function flee(){
+function flee() {
   playerFlee = true;
-  if(playerFlee){
+  if (playerFlee) {
     playerFlee = false;
     gameOver();
   }
   console.log(playerFlee);
+  alert ("Cowards have no place in a game like this!🐔");
 }
 
-function enemyAttack(){
+function enemyAttack() {
   playerHP = playerHP - enemyAP;
+  console.log(playerHP);
 
-  if(playerHP <= 0){
+  if (playerHP <= 0) {
     gameOver();
-  } 
+    alert ("The enemy has defeated you.")
+  }
 }
 
-function nextLevel(){
-   playerHP = 100;
+function nextLevel() {
+  playerHP = 100;
 
-   enemyIndex++;
-   enemyAP = enemyList[enemyIndex].attack;
-   enemyHP = enemyList[enemyIndex].health;
-   enemyName = enemyList[enemyIndex].name;
+  enemyIndex++;
+  enemyAP = enemyList[enemyIndex].attack;
+  enemyHP = enemyList[enemyIndex].health;
+  enemyName = enemyList[enemyIndex].name;
+  const textPrompts = [
+    "The old man promised you treasure. He also promised you danger. So far, it has been mostly the latter...",
+    "Maybe your friend is around this next turn...",
+    "What's that? Who's there!?",
+    "You press onwards. You must escape. As unlikely as that seems...",
+    "I  want to be an adventurer, you said. It'll be fun, you said.",
+    "What did I just step in? Ew! It's on my boots! EEEWWW!"]
 
-   //text[enemyIndex].object
+  const randomPrompt = textPrompts[Math.floor(Math.random() * textPrompts.length)];
+  alert (randomPrompt);
+
+  //text[enemyIndex].object
 }
 
-function gameOver(){
+function gameOver() {
   window.location.href = 'gameOver.html';
 }
 
-
-
-
-userAttack.on("click", function(e){
+userAttack.on("click", function (e) {
   e.preventDefault();
   attack();
 });
 
-userHeal.on("click", function(e){
+userHeal.on("click", function (e) {
   e.preventDefault();
   heal();
 });
 
-userFlee.on("click", function(e){
+userFlee.on("click", function (e) {
   e.preventDefault();
   flee();
 });
